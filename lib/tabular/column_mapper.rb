9e46a48b014a800619ac74b0716337b7beff0504
Scott Willson <scott.willson@gmail.com>
2015-11-17 19:06:53 -0800

    Use #delete instead of #gsub

--- lib/tabular/column_mapper.rb
+++ lib/tabular/column_mapper.rb
@@ -15,7 +15,7 @@ module Tabular
           gsub(/([a-z\d])([A-Z])/,'\1_\2').
           tr("-", "_").
           gsub(/ +/, "_").
-          gsub(";", "").
+          delete(";").
           downcase.
           to_sym
       rescue
